d90cd072e1ce0d5626f60aaa0c99d914f0152e83
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-04-23 14:18:20 -0700

    feature(app-routes): creating app-routes component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <div id="navbar">
+            <div className="row text-center">
+                <div className="col">
+                    <h2>Nav</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ const HomePage = () => {
     return (
         <div id="home">
             <div className="row text-center">
-                <div className="co">
+                <div className="col">
                     <h2>Home Page!</h2>
                 </div>
             </div>
