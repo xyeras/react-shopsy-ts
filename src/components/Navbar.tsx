1e880c1caa624839f292744bced0b063df8b2a38
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-04-23 15:01:04 -0700

    feature(app-routes): import navbar into app-routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,32 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-    return (
-        <div id="navbar">
-            <div className="row text-center">
-                <div className="col">
-                    <h2>Nav</h2>
-                </div>
-            </div>
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+      {/* Logo */}
+      <Link to='/' className='navbar-brand'>
+        Shopsy
+      </Link>
+
+      {/* Menu */}
+      <div className='collapse navbar-collapse'>
+        <div className='navbar-nav'>
+          <Link className='nav-link' to='/'>
+            Home
+          </Link>
         </div>
-    );
-}
+      </div>
+
+      {/* Cart Icon */}
+      <div className='sh-cart-icon'>
+        <div className='navbar-nav'>
+          <Link to='/cart' className='nav-link'>
+            Cart
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/routers/AppRoutes.tsx
+++ src/routers/AppRoutes.tsx
@@ -4,18 +4,23 @@ import HomePage from '../pages/HomePage';
 import CartPage from '../pages/CartPage';
 import CheckoutPage from '../pages/CheckoutPage';
 import ProductPage from '../pages/ProductPage';
+// Components
+import Navbar from '../components/Navbar';
 
 const AppRoutes = () => {
-    return (
-        <div id="app-routes">
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/products/:productId" component={ProductPage} />
-                <Route path="/cart" component={CartPage} />
-                <Route path="/checkout" component={CheckoutPage} />
-            </Switch>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Navbar />
+      <div className='container'>
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/products/:productId' component={ProductPage} />
+          <Route path='/cart' component={CartPage} />
+          <Route path='/checkout' component={CheckoutPage} />
+        </Switch>
+      </div>
+    </div>
+  );
+};
 
 export default AppRoutes;
