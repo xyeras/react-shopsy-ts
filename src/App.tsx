75cdfc1c230cc1643428ecf8561b4c650f40a329
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-04-23 11:55:20 -0700

    chore(env-setup): removed unneeded files and finished setup of initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row text-center'>
+        <div className='col'>
+          <h2>This is my App!</h2>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
