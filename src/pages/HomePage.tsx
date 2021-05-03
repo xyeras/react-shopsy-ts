6ee5383a200197236856b470534482f72bf273da
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-05-03 14:09:19 -0700

    feat(prod-card): create component for product card and import to home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import ProductCard from '../components/ProductCard';
 import { GlobalContext } from '../context/GlobalContext';
 import '../App.css';
 
@@ -19,43 +19,13 @@ const HomePage = () => {
         </div>
       </div>
       <div className='row'>
-            {products.map((product, i) => {
-              return (
-                <div className="col-sm-12 col-md-3 mb-3" key={i}>
-                  <div className="card h-100">
-                    {/* Image on Top */}
-                    <div className="sh-card-img">
-                        <div className="sh-bg-img" style={{backgroundImage: `url(${product.image})` }}>
-
-                      </div>
-                    </div>
-                    
-                    {/* Card Body  */}
-                    <div className="card-body">
-                        <h5 className="card-title">
-                        <Link to={`/products/${product.id}`}>
-                          {product.title}
-                        </Link>
-                        </h5>
-                      <div className="d-flex justify-content-between align-items-center">
-                        <strong>${product.price}</strong>
-                        <span className="badge badge-warning">
-                          {product.category}
-                        </span>{' '}
-                      </div>
-                    </div>
-
-                    {/* Add to Cart Button */}
-                    <div className="card-footer">
-                      <button className="btn btn-block btn-primary">
-                        Add To Cart
-                      </button>
-                    </div>
-
-                  </div>
-                </div>
-              );
-            })}
+        {products.map((product, i) => {
+          return (
+            <div className='col-sm-12 col-md-3 mb-3' key={i}>
+              <ProductCard product={product} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
