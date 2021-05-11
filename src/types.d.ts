6a3041766d44c8afed18fdac504ba5eb5af60a6f
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-05-11 06:31:41 -0700

    refac (cart-item): added cartItem type, updated add_to_cart function in reducer

--- src/types.d.ts
+++ src/types.d.ts
@@ -17,11 +17,43 @@ type Product = {
     description: string;
     image: string;
 }
+/*
+- For javascript you would create a data model by creating a class for a data type
+
+class Product {
+    constructor(id, title, price, category, description, image) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+}
+*/
+
+/*
+Return for cartItem
+{
+    id: 1,
+    quantity: 2,
+    product: {
+        id: 1,
+        title: 'mens shirt',
+        category: 'mens clothing',
+        description:
+    }
+}
+*/ 
+type CartItem = {
+    id: number;
+    quantity: number;
+    product:Product;
+}
+
+
 
 // Define a state type
 type InitialStateType = {
     products: Product[];
-    cart: Product[];
+    cart: CartItem[];
     product: Product | undefined;
     is_loading: boolean;
     getProducts: () => void;
